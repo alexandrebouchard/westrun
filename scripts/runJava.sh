d844dc249408aa5da1c062ce78def174fd8d3ef9
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-06-15 09:57:11 -0700

    Add main bash entry points

--- scripts/runJava.sh
+++ scripts/runJava.sh
@@ -10,10 +10,17 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  
-  echo $DIR
-  echo Main is: $1
+
+  # Prepare main variables used to do the java call
+  MAIN=$1
   shift 1
-  echo Arguments: $@
+  ARGS=$@
+  
+  # Simple script to run the java program from the eclipse-generated .classpath file
+  REP_ROOT=$DIR/../
+  CLASSPATH=${REP_ROOT}/bin/:`cat ${REP_ROOT}/.classpath | grep jar | sed 's/.* path..//' | sed 's/\".*//' | tr "\n" ":"`
+  CLASSPATH=${CLASSPATH}:`cat ${REP_ROOT}/.classpath | grep combineaccessrules | sed "s#.* path..#${REP_ROOT}/..#" | sed 's#\".*#/bin/#' | tr "\n" ":"`
+
+  java -Xmx2g -cp ${CLASSPATH} ${MAIN} ${ARGS}
 
 }
